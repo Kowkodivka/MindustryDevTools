75131136155c3bb4129223206f7bd89f490df021
Kowkodivka <theeugenet@gmail.com>
2023-05-26 14:13:46 +0700

    legacy porn

--- network/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-val mindustryVersion = ext.get("mindustry-version")
-
-dependencies {
-    compileOnly("com.github.Anuken.Arc:arc-core:$mindustryVersion")
-    compileOnly("com.github.Anuken.MindustryJitpack:core:$mindustryVersion")
-}
